893bf50180fc2ba6cd2488865e5b343705f56669
Cat <ccase2008@yahoo.com>
2018-04-04 19:48:18 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("This file is linked");
+var teacher = "Sally Jones";
+var department = "Physics";
+var ratings = "3.4, 5.0, 4.2";
+console.log("Teacher: Sally Jones");
+console.log("Department: Physics");
+console.log("Ratings: 3.4, 5.0, 4.2");
+var avgRating = "4.2";
+console.log("Avg Rating: 4.2");
+
+var student = "Sally Jones";
+var course = "Calculus";
+var gpa = "6.0";
+console.log("Student: Sally Jones");
+console.log("Course: Calculus");
+console.log("GPA: 6.0");
