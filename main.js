f141fc1df45068015cdea260f45074f0b25ff1b6
Cat Case <35554104+catcase@users.noreply.github.com>
2019-05-09 23:26:38 -0500

    Updated files to new location

--- main.js
+++ main.js
@@ -1,70 +1,42 @@
+// log message to console to make sure page is linked
+console.log('this page is linked!');
 
 
-// var getRatingAvg = ['3.5', '1.0', '5.0'];
+// create variables for a teacher name, department name, and three ratings
+var teacher = 'Teacher: Sally Jones';
+
+var dept = 'Department: Physics';
+
+var ratingOne = 4.3;
+var ratingTwo = 2.5;
+var ratingThree = 5.0;
+
+console.log(teacher);
+console.log(dept);
+console.log(ratingOne, ratingTwo, ratingThree);
 
 
-function getRatingAvg(ratings) {
-  // add all ratings in ratings array to sum
-  var sum = 0;
-  // var sum = 3.5 + 1.0 + 5.0;
-  // var sum = ratings[0] + ratings[1] + ratings[2];
-  // sum = sum + ratings[0];
-  // sum = sum + ratings[1];
-  // sum = sum + ratings[2];
+// calculate average rating by adding ratings together, & divide by three
+var avgRating = (ratingOne + ratingTwo + ratingThree) / 3;
 
-  for (var i = 0; i <= 2; i++) {
-    var rating = ratings[i];
-    sum = sum + rating;
-  }
+console.log(avgRating);
 
-  var avg = sum / ratings.length
 
-  // todo: divide sum by array.length to get avg rating
+// round the average rating to the nearest 10th decimal
+avgRating = Math.round(avgRating * 10) / 10;
 
-  console.log(sum);
+console.log(avgRating);
+
+
+// store rating variables into one array
+var ratingArray = [ratingOne, ratingTwo, ratingThree];
+
+
+// create function to calculate & return avg rating from array
+function getRatingAvg (ratingArray) {
+  return ratingOne + ratingTwo + ratingThree / 3;
 }
 
-getRatingAvg([3.5, 1.0, 5.0]);
+var finalRating = getRatingAvg();
 
-var teacherName = "Sally Jones"
-var ratingsArray = [3.5, 1.0, 5.0]
-
-function addTeacherRating(ratingsArray, newRating) {
-  console.log(newRating)
-  ratingsArray.push()
-  console.log(ratingsArray)
-}
-addTeacherRating(ratingsArray, 2.6)
-
-
-/* var ratingAvg = prompt("We would like for you to review. Please enter a number between 1 - 5");
-
-
-while (ratingAvg > 5) {
-  ratingAvg = prompt("Please enter a number between 1-5");
-}
-addTeacherRating(ratingsArray, ratingAvg)
-alert("Thanks for your review! " + teacherName + " new rating is " + ratingAvg) */
-
-
-var course = [
-  ["Physics", "Science" ],
-  ["Calculus", "Math"],
-  ["Econ101", "Social Science"],
-  ["Astronomy", "Science"]
-];
-
-function courseFilters(course, dept) {
-  var filter = course.filter(item => item[1] === dept);
-  return filter
-}
-
-var filteredCourses = courseFilters(course, "Science")
-var filteredCourses2 = courseFilters(course, "Math")
-var filteredCourses3 = courseFilters(course, "Social Science")
-
-console.log(filteredCourses)
-console.log(filteredCourses2)
-console.log(filteredCourses3)
-
-var department = prompt("What Department would you like to access?");
+console.log(finalRating);
